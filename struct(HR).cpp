a732b88d398b9b760587ea762100688c912de544
anshdeep <anshd258@gmail.com>
2022-07-31 15:17:37 +0530

    struct progran (HR) level:easy

---
+++ struct(HR).cpp
@@ -0,0 +1,43 @@
+/* nput will consist of four lines.
+The first line will contain an integer, representing age.
+The second line will contain a string, consisting of lower-case Latin characters ('a'-'z'), representing the first_name of a student.
+The third line will contain another string, consisting of lower-case Latin characters ('a'-'z'), representing the last_name of a student.
+The fourth line will contain an integer, representing the standard of student.
+
+Note: The number of characters in first_name and last_name will not exceed 50.
+
+OUTPUT
+
+nput will consist of four lines.
+The first line will contain an integer, representing age.
+The second line will contain a string, consisting of lower-case Latin characters ('a'-'z'), representing the first_name of a student.
+The third line will contain another string, consisting of lower-case Latin characters ('a'-'z'), representing the last_name of a student.
+The fourth line will contain an integer, representing the standard of student.
+
+Note: The number of characters in first_name and last_name will not exceed 50.
+*/
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct Student{
+    int age;
+    string first_name;
+    string last_name;
+    int standard;
+    
+};
+
+int main() {
+    Student st;
+    
+    cin >> st.age >> st.first_name >> st.last_name >> st.standard;
+    cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
+    
+    return 0;
+}
