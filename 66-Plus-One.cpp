ea14ce3b16b309c623cb728e4606cc030473ecc1
anshdeep <anshd258@gmail.com>
2022-08-11 03:45:41 +0530

    modified program 66

---
+++ 66-Plus-One.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for (int i = digits.size() - 1; i >= 0; --i) {
+      if (digits[i] < 9) {
+        ++digits[i];
+        return digits;
+      }
+      digits[i] = 0;
+    }
+
+    digits.insert(begin(digits), 1);
+    return digits;
+  
+    }
+};
